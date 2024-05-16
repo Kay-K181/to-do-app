3b258656ce02ca81032332756c48bc27639c954a
kay-k181 <kaykaywill@live.co.uk>
2024-05-16 15:53:32 +0100

    fourth commit

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ToDoList from "./ToDoList";
-import './App.css'
 
 export default function App(){
   return(
-    <>
-      <ToDoList />
-    </>
+    <ToDoList />
   )
 }
